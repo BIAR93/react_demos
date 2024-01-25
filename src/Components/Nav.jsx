99982383eab582b06bd069c4909271262956f841
BIAR93 <beatriz.alves-ribeiro@mail.com>
2024-01-25 18:37:13 +0000

    demo

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <>
+        <a>Home</a>
+        <a>About</a>
+        <a>Contact</a>
+      </>
+    );
+  }
+}
+
+export default Nav;
